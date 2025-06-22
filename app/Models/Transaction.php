572b044e4eebcbcd86c74bdf95ad18a0402baeab
Kevin Sinatria <kevinsinatria8@gmail.com>
2025-06-22 11:45:31 +0700

    feat: update model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     protected $table = 'transactions';
+
+    protected $fillable = [
+        'user_id',
+        'type',
+        'amount',
+        'description',
+        'transaction_date',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
