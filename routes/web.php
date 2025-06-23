f16868081614282bf21b0fdf2e2d9b38015e77f1
Kevin Sinatria <kevinsinatria8@gmail.com>
2025-06-23 13:08:03 +0700

    feat: kategori yang bisa diakses oleh admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use Google\Client;
+use Google\Service\Drive;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/oauth', function () {
+    $client = new Client();
+    $client->setClientId(env('GOOGLE_DRIVE_CLIENT_ID'));
+    $client->setClientSecret(env('GOOGLE_DRIVE_CLIENT_SECRET'));
+    $client->setRedirectUri(env('GOOGLE_DRIVE_REDIRECT_URI')); // Pastikan ini sama dengan yang di GC Console
+    $client->addScope(Drive::DRIVE); // Scope untuk akses Google Drive
+    $client->setAccessType('offline'); // Penting untuk mendapatkan refresh token
+    $client->setPrompt('select_account consent'); // Penting untuk mendapatkan refresh token setiap kali
+
+    if (!request()->has('code')) {
+        // Redirect user to Google for authorization
+        return redirect($client->createAuthUrl());
+    } else {
+        // Handle callback from Google
+        $accessToken = $client->fetchAccessTokenWithAuthCode(request('code'));
+        // Simpan refresh_token ini! Ini yang kamu butuhkan.
+        dd($accessToken['refresh_token']);
+        // Setelah mendapatkan refresh_token, hapus route ini
+    }
 });
